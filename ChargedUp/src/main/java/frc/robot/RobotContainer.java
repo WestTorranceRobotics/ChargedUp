cdf5c078c271baf19046dd2378c187e673b72016
Pineapple131 <92963579+Pineapple131@users.noreply.github.com>
2023-01-11 16:31:11 -0800

    made robotmap

--- ChargedUp/src/main/java/frc/robot/RobotContainer.java
+++ ChargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.RobotMap.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
