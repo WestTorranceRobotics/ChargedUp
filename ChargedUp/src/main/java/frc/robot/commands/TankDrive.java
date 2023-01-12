c66e2db53656cc7926c13090603b3179159f00ab
Pineapple131 <92963579+Pineapple131@users.noreply.github.com>
2023-01-11 16:31:39 -0800

    did i push

--- ChargedUp/src/main/java/frc/robot/commands/TankDrive.java
+++ ChargedUp/src/main/java/frc/robot/commands/TankDrive.java
@@ -19,7 +19,6 @@ public class TankDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    int x = 0;
   }
 
   // Called once the command ends or is interrupted.
