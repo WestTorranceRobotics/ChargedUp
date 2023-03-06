eeb3a79a3a50f7cfebf29cf791415f012c0c0301
CyberYeti <yitmengchin@gmail.com>
2023-03-05 18:00:08 -0800

    Lol haha

--- ChargedUp/src/main/java/frc/robot/RobotContainer.java
+++ ChargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -97,6 +97,8 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
 
+  XboxController controller = new XboxController(1);
+
   // Replace with CommandPS4Controller or CommandJoystick if needed
   //private final CommandXboxController m_driverController =
       //new CommandXboxController(OperatorConstants.kDriverControllerPort);
